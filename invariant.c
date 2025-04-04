010090fc1ac9fbce0a4fbf6195cf55e314435fbc
Aryaman Sonkiya <asonkiya@unc.edu>
2025-04-03 20:09:21 -0400

    Create invariant.c

---
+++ invariant.c
@@ -0,0 +1,19 @@
+// Loop invariant verification
+#include <klee/klee.h>
+#include <assert.h>
+
+int main() {
+    int n;
+    klee_make_symbolic(&n, sizeof(n), "n");
+    klee_assume(n >= 1);
+    klee_assume(n <= 10);
+
+    int prod = 1;
+    for (int i = 1; i <= n; i++) {
+        prod *= i;
+        // Invariant: product should always be divisible by i
+        klee_assert(prod % i == 0);
+    }
+
+    return 0;
+}
