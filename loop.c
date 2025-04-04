aa4a63a7252ea7a9e9d434fe2446556121579c2e
Aryaman Sonkiya <asonkiya@unc.edu>
2025-04-03 20:08:21 -0400

    Create loop.c

---
+++ loop.c
@@ -0,0 +1,22 @@
+// Loop behaivior exploration
+#include <klee/klee.h>
+#include <assert.h>
+
+int main() {
+    int x;
+    klee_make_symbolic(&x, sizeof(x), "x");
+    klee_assume(x >= 0);
+    klee_assume(x <= 5); // bounded loop
+
+    int count = 0;
+    for (int i = 0; i < x; i++) {
+        count++;
+    }
+
+    // Trigger exploration of loop-unroll variants
+    if (count == 3) {
+        klee_assert(1); // reachable for x == 3
+    }
+
+    return 0;
+}
